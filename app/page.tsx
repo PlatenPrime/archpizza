b66dc80761a578224c061bd409948cfbfa30592b
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-08-21 09:01:06 +0300

    21.08.2024

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <h1>Home
+
+  <Button
+  variant="outline"
+  >button</Button>
+ </h1>
+  );
+}
