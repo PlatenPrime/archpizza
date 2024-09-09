685a90e21d260932ea24fdf50632260ef3194696
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-09-09 08:13:57 +0300

    09.09.2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,65 @@
+"use client"
+
+import { Alert } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <h1 className="">
+    <div className="">
       Home
+      <Button>Click</Button>
+      <Input placeholder="Enter your name" />
+      <Alert variant={"destructive"} >You do sng wrong</Alert>
 
-      <Button variant="destructive">Hello</Button>
-    </h1>
+
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => { window.alert("Continue") }} >Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+
+
+
+
+      <div className="h-20 w-20">
+        <AspectRatio ratio={16 / 9} className="bg-muted">
+          <Image
+            src="https://sharik.ua/upload/elements_big/1101-0001_m1.jpg"
+            alt="Photo by Drew Beamer"
+            fill
+            className="h-full w-full rounded-md object-cover"
+          />
+        </AspectRatio>
+      </div>
+
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',  // Разрешает все пути для данного хоста
+            },
+
+            {
+                protocol: 'https',
+                hostname: 'sharik.ua',
+                port: '',
+                pathname: '/**',  // Разрешает все пути для данного хоста
+            },
+
+        ],
+    },
+};
 
 export default nextConfig;
